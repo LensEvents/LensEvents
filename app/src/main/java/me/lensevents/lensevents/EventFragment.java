998382c84f8e26c23ca271d7bcc2988e8370ee13
Javier Bonilla <javierbonillad@gmail.com>
2017-05-03 18:43:12 +0200

    feat: event list not working properly

---
+++ app/src/main/java/me/lensevents/lensevents/EventFragment.java
@@ -0,0 +1,98 @@
+package me.lensevents.lensevents;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.lensevents.model.Event;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EventFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EventFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EventFragment extends Fragment {
+
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private int mColumnCount = 1;
+    private OnFragmentInteractionListener mListener;
+
+    public EventFragment() {
+        // Required empty public constructor
+    }
+
+    public static EventFragment newInstance(int columnCount) {
+        EventFragment fragment = new EventFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_event_list, container, false);
+
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+            EventRecyclerViewAdapter eventRecyclerViewAdapter = new EventRecyclerViewAdapter(mListener);
+            recyclerView.setAdapter(eventRecyclerViewAdapter);
+        }
+
+        return inflater.inflate(R.layout.fragment_event_list, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Event event);
+    }
+}
---
+++ app/src/main/java/me/lensevents/lensevents/EventRecyclerViewAdapter.java
@@ -0,0 +1,133 @@
+package me.lensevents.lensevents;
+
+import android.graphics.Bitmap;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import me.lensevents.lensevents.EventFragment.OnFragmentInteractionListener;
+import me.lensevents.model.Event;
+
+public class EventRecyclerViewAdapter extends RecyclerView.Adapter<EventRecyclerViewAdapter.ViewHolder> {
+
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mValueEventListener;
+    private final OnFragmentInteractionListener mListener;
+
+    private List<Event> mEvents = new ArrayList<>();
+    private List<String> mEventsIds = new ArrayList<>();
+
+    public EventRecyclerViewAdapter(OnFragmentInteractionListener listener) {
+        mListener = listener;
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("Events");
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Event event = dataSnapshot.getValue(Event.class);
+                mEventsIds.add(dataSnapshot.getKey());
+                mEvents.add(event);
+                notifyItemInserted(mEvents.size() - 1);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                Event event = dataSnapshot.getValue(Event.class);
+                int index = mEventsIds.indexOf(dataSnapshot.getKey());
+                mEvents.set(index, event);
+                notifyItemChanged(index);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                int index = mEventsIds.indexOf(dataSnapshot.getKey());
+                mEvents.remove(index);
+                mEventsIds.remove(index);
+                notifyItemRemoved(index);
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(childEventListener);
+        mValueEventListener = childEventListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_event, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mEvent = mEvents.get(position);
+        Event event = mEvents.get(position);
+        holder.mNameView.setText(event.getName());
+
+        Bitmap bitmap = null;
+        RequestForImageTask requestForImageTask = new RequestForImageTask();
+        try {
+            bitmap = requestForImageTask.execute(event.getPhoto()).get();
+        } catch (InterruptedException | ExecutionException e) {
+            Log.getStackTraceString(e);
+        }
+
+        holder.mImageView.setImageBitmap(bitmap);
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onFragmentInteraction(holder.mEvent);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mEvents.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView mImageView;
+        public final TextView mNameView;
+        public Event mEvent;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mImageView = (ImageView) view.findViewById(R.id.event_image);
+            mNameView = (TextView) view.findViewById(R.id.event_name);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mNameView.getText() + "'";
+        }
+    }
+}
