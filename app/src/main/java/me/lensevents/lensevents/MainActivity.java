ba9c0c4146289d01df3e6bbf8bd7b174e69bb93e
Daniel Moreno <danimoreno.m08@gmail.com>
2017-05-19 20:31:56 +0200

    fix: fixed sing in

--- app/src/main/java/me/lensevents/lensevents/MainActivity.java
+++ app/src/main/java/me/lensevents/lensevents/MainActivity.java
@@ -3,6 +3,7 @@ package me.lensevents.lensevents;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -68,8 +69,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
                                 }
                             });
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.putExtra("fromLogout", true);
+                    finish();
+                    startActivity(intent);
                 }
-                initFragments();
             }
         }
         if (requestCode == CreateGroupFragment.REQUEST_CODE) {
@@ -86,6 +91,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             createGroupFragment.onActivityResult(requestCode, resultCode, data);
         }
     }
+
 
     private void saveUserData() {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
